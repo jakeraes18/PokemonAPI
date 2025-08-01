59f69cced36b2b698083131491822c126b4eb5ef
Jason Ramirez <jake.raes18@gmail.com>
2025-07-31 22:32:41 -0600

    Subir aplicacion a git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PokemonSearchComponent } from './components/pokemon-search/pokemon-search.component'; // si lo usas directamente
+import { PokemonSearchComponent } from './components/pokemon-search/pokemon-search.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [RouterOutlet] // ← agrega RouterOutlet aquí
+  imports: [RouterOutlet]
 })
 export class AppComponent {}
